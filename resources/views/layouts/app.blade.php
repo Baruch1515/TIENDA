d1d884730d5e333ff07d35426de322a9bba0608b
Baruch13 <ernestobaruch15@gmail.com>
2021-04-29 12:58:27 -0500

    barra

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,18 +30,9 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" style="margin: 15px;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Categorias
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-  @foreach($categorias as $categoria)
-    <a class="dropdown-item" href="{{ $categoria['id'] }}">{{ $categoria['nombre'] }}</a>
-    @endforeach
-  </div>
-</div>
+                
+  
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                   
                                     
